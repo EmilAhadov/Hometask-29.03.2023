9a9f80e5854810398f3d6539157c9141294ab298
EmilAhadov <emilehedov16@gmail.com>
2023-03-29 08:35:03 +0400

    Homework is ready

---
+++ gameTask/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //--------------------------------------------------------------------------------
+
+        //byte @byte = 233;
+        //sbyte @sbyte = 100;
+        //short @short = -20000;
+        //ushort @ushort = 40000;
+        //int @int = -2000000;
+        //uint @uint = 4000000;
+        //long @long = 300000000;
+        //ulong @ulong = 600000000;
+        //float @float = 0.324f;
+        //double @double = 0.3453463;
+        //decimal @decimal = 233425.32545234534m;
+        //char @char = 'a';
+        //string @string = "Something";
+        //bool @bool = true;
+
+        //--------------------------------------------------------------------------------
+
+        //float variable1 = 353.3453f;
+        //int variable2 = (int)variable1;
+        //Console.WriteLine(variable2);
+
+        //--------------------------------------------------------------------------------
+
+        //Console.Write("Please enter a number: ");
+        //int number = int.Parse(Console.ReadLine()); 
+
+        //if (number == 0)
+        //{
+        //    Console.WriteLine("Number is zero");
+        //}
+        //else if (number > 0)
+        //{
+        //    Console.WriteLine("Number is positive");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Number is negative");
+        //}
+
+        //--------------------------------------------------------------------------------
+
+        //Console.WriteLine("Please enter your age: ")
+        //int age = int.Parse(Console.ReadLine());
+        //if(age>=18)
+        //{
+        //    Console.WriteLine("You can vote");
+        //}
+        //else 
+        //{
+        //    Console.WriteLine("You can't vote"); 
+        //}
+
+
+        //--------------------------------------------------------------------------------
+
+        //for(int i = 2; i<=100;  i+=2) 
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        //--------------------------------------------------------------------------------
+
+        //Console.Write("Please enter the number: ");
+        //int number = int.Parse(Console.ReadLine());
+        //for (int i = 1 ; i <= 10  ; i++)
+        //{
+        //    Console.WriteLine(number*i);
+        //}
+
+        //--------------------------------------------------------------------------------
+
+        //string password = "password";
+        //Console.Write("Please enter the password: ");
+        //string userInput = Console.ReadLine();  
+        //while (userInput != password) 
+        //{
+        //    Console.Clear();
+        //    Console.ForegroundColor = ConsoleColor.Red;
+        //    Console.WriteLine("Wrong password. Please try again");
+        //    Console.ReadKey();
+        //    Console.ForegroundColor = ConsoleColor.White;
+        //    Console.Clear();
+        //    Console.Write("Please enter the password: ");
+        //    userInput = Console.ReadLine();
+        //}
+        //Console.Clear();
+        //Console.ForegroundColor = ConsoleColor.Green;
+        //Console.WriteLine("Password is correct. Welcome");
+
+        //--------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------
+    }
+}
